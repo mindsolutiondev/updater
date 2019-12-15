593cbf8a15baca3b1e5a5479937597bd9d01aa6b
topspinppy <5806021623049@fitm.kmutnb.ac.th>
2019-12-15 23:20:41 +0700

    changeport

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-let port = 9000 || process.env.PORT
 app.use('/static',express.static('update'))
 
 app.get('/', (req, res) => {
   res.json({ message: 'mind solution updater' })
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log('Application is running on port 9000')
 })
