35ac39d8668eb824dd66befe843beb20af1e4b98
topspinppy <5806021623049@fitm.kmutnb.ac.th>
2019-12-15 23:16:48 +0700

    fix port

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-
+let port = 9000 || process.env.PORT
 app.use('/static',express.static('update'))
 
 app.get('/', (req, res) => {
   res.json({ message: 'mind solution updater' })
 })
 
-app.listen(9000 || process.env.PORT , () => {
+app.listen(port, () => {
   console.log('Application is running on port 9000')
 })
